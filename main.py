da2682fa8a0d962f67d9aeee2ba59ddadad05d9a
abbirosen <abni1482@colorado.edu>
2020-04-15 17:07:50 -0600

    made image url input

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from client import Client
 from worker.sqlite import Db_Connection
+import io
+import sys
 
 def main():
     # database connection
@@ -7,8 +9,12 @@ def main():
     singleton_db = db_conn.get_instance()
     print(singleton_db)
 
+    # user input for image url
+    # testing with 'https://sc01.alicdn.com/kf/HTB1FM3eLXXXXXadXXXXq6xXFXXXC/teddy-bear-stuff-toys.jpg_350x350.jpg'
+    img_url = sys.argv[1]
+
     # Client
-    client = Client('https://sc01.alicdn.com/kf/HTB1FM3eLXXXXXadXXXXq6xXFXXXC/teddy-bear-stuff-toys.jpg_350x350.jpg')
+    client = Client(img_url)
     result = client.to_worker()
     print(result)
 
